b3104bb293966cf3779b36661004f1042cdd8ee0
Damian Zaremba <damian@damianzaremba.co.uk>
2015-08-22 17:55:19 +0100

    Update

--- fabfile.py
+++ fabfile.py
@@ -72,6 +72,9 @@ def start():
 
 
 def update_code():
+    print('Resetting local changes')
+    sudo('cd "%(dir)s" && git reset --hard && git clean -fd' % {'dir': DEST_DIR})
+
     print('Updating code')
     sudo('cd "%(dir)s" && git pull origin master' % {'dir': DEST_DIR})
 
