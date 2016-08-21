6869371b3efe5b0890765ce762b4357862473ca7
Damian Zaremba <damian@damianzaremba.co.uk>
2016-08-21 13:39:05 +0200

    Fix application name

--- bin/run_bot.sh
+++ bin/run_bot.sh
@@ -3,13 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 cd $DIR/
 if [ "$(whoami)" == "tools.cluebotng" ];
 then
-    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "bot")'
 else
     if [ "$(whoami)" == "tools.cluebot" ];
     then
-        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "bot")'
     else
-        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s53115__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s53115__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "bot")'
     fi
 fi
 exec /usr/bin/php -f $DIR/bot/cluebot-ng.php
