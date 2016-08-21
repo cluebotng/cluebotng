59f9e7672863da3273fdd8b5590695a3bf19c261
Damian Zaremba <damian@damianzaremba.co.uk>
2016-08-21 13:38:01 +0200

    Fix paths + deal with 3 accounts temp

--- bin/run_bot.sh
+++ bin/run_bot.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 cd $DIR/
-mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "bot")'
+if [ "$(whoami)" == "tools.cluebotng" ];
+then
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+else
+    if [ "$(whoami)" == "tools.cluebot" ];
+    then
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+    else
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s53115__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+    fi
+fi
 exec /usr/bin/php -f $DIR/bot/cluebot-ng.php
--- bin/run_relay.sh
+++ bin/run_relay.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 cd $DIR/relay_irc/
-mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
+if [ "$(whoami)" == "tools.cluebotng" ];
+then
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
+else
+    if [ "$(whoami)" == "tools.cluebot" ];
+    then
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
+    else
+        mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s53115__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
+    fi
+fi
 exec /data/project/cluebot/node/bin/node relay_irc.js
