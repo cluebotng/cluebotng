a1e3c3b528c4078ad1e2ddb77e96b65a1d37aa7b
Damian Zaremba <damian@damianzaremba.co.uk>
2017-03-04 14:30:05 +0100

    Update mysql dump

--- bin/backup_database.sh
+++ bin/backup_database.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 mkdir -p $HOME/mysql_backups
-
 filename=`date +"%d-%m-%Y_%H-%M-%S"`
-mysqldump  --defaults-file="${HOME}"/replica.my.cnf -h tools-db s51109__cb > "$HOME/mysql_backups/$filename-cb.sql"
+if [ "$(whoami)" == "tools.cluebot" ];
+then
+    mysqldump  --defaults-file="${HOME}"/replica.my.cnf -h tools-db s51109__cb > "$HOME/mysql_backups/$filename-cb.sql"
+fi
+if [ "$(whoami)" == "tools.cluebotng" ];
+then
+    mysqldump  --defaults-file="${HOME}"/replica.my.cnf -h tools-db s52585__cb > "$HOME/mysql_backups/$filename-cb.sql"
+fi
