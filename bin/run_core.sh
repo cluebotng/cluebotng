47877286a73491d6d45bfd162f8bfde1bb1044cd
Damian Zaremba <damian@damianzaremba.co.uk>
2017-03-04 12:53:12 +0100

    really fix cluster entries

--- bin/run_core.sh
+++ bin/run_core.sh
@@ -3,10 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 cd $DIR/
 if [ "$(whoami)" == "tools.cluebot" ];
 then
-    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s51109__interface -e 'replace into `cbng_backend_clusternode` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
 fi
 if [ "$(whoami)" == "tools.cluebotng" ];
 then
-    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__interface -e 'replace into `cbng_backend_clusternode` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "core")'
 fi
 exec ./cluebotng -l -m live_run
--- bin/run_redis.sh
+++ bin/run_redis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
-cd $DIR/relay_redis/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+cd $DIR/
 if [ "$(whoami)" == "tools.cluebot" ];
 then
-    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb -A s51109__interface -e 'replace into `cbng_backend_clusternode` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "redis")'
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s51109__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
 fi
 if [ "$(whoami)" == "tools.cluebotng" ];
 then
-    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__cb -e 'replace into `cbng_backend_clusternode` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "redis")'
+    mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
 fi
 exec /data/project/cluebot/node/bin/node relay_redis.js
