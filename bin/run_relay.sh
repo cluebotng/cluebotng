1b4e3d82c26392db7d650cc63d042e6175c23cf7
Damian Zaremba <damian@damianzaremba.co.uk>
2017-03-04 13:17:57 +0100

    Move apps path

--- bin/run_relay.sh
+++ bin/run_relay.sh
@@ -9,5 +9,6 @@ fi
 if [ "$(whoami)" == "tools.cluebotng" ];
 then
     mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
+    cd /data/project/cluebotng/apps/bot/relay_irc
     exec /data/project/cluebotng/node/bin/node relay_irc.js
 fi
