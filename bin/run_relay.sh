8bb4d2cbf7cf1e5b07ef754dd2d67d4935a64d4b
Damian Zaremba <damian@damianzaremba.co.uk>
2018-09-22 11:06:50 -0700

    run_relay.sh - save logs

--- bin/run_relay.sh
+++ bin/run_relay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mysql --defaults-file="${HOME}"/replica.my.cnf -h tools-db -A s52585__cb -e 'replace into `cluster_node` values ("'$(hostname -f | sed 's/[^A-Za-z0-9\._\-]+//g')'", "relay")'
 cd /data/project/cluebotng/apps/bot/relay_irc
-exec /data/project/cluebotng/node/bin/node relay_irc.js
+exec /data/project/cluebotng/node/bin/node relay_irc.js >> ~/logs/relay_irc.log
